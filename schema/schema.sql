8cf52ecbf2b753711e2e9b81f9989960ecf13c04
lairdm <lairdm@9485699e-71d8-0410-94f2-999dd83c1c5b>
2013-03-22 06:58:00 +0000

    initial examples done

--- schema/schema.sql
+++ schema/schema.sql
@@ -7,6 +7,9 @@ CREATE TABLE IF NOT EXIST task (
   config_type VARCHAR(15) not null,
   extra_parameters VARCHAR(30),
   priority int unsigned default 2,
+  submitted_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  start_date TIMESTAMP NOT NULL,
+  complete_date TIMESTAMP NOT NULL,
   PRIMARY KEY (task_id)
 );
 
@@ -16,15 +19,19 @@ CREATE TABLE IF NOT EXIST component (
   run_status enum('PENDING','COMPLETE','HOLD','ERROR','RUNNING'),
   extra_parameters VARCHAR(30),
   qsub_file VARCHAR(100),
-  qsub_id int unsigned
-
+  qsub_id int unsigned,
+  start_date TIMESTAMP NOT NULL,
+  complete_date TIMESTAMP NOT NULL,
+  PRIMARY KEY (component_id)
 );
 
 CREATE TABLE IF NOT EXIST mail (
   mail_id int unsigned NOT NULL auto_increment,
   task_id int unsigned NOT NULL,
   email varchar(40) NOT NULL,
-  sent tinyint(1) DEFAULT 0,
-  
+  sent bool DEFAULT 0,
+  added_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  sent_date TIMESTAMP NOT NULL,
+  PRIMARY KEY (mail_id)
 );
 
