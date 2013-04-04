f7286158f7bae77db3a9e4173265bc39ed1fb902
lairdm <lairdm@9485699e-71d8-0410-94f2-999dd83c1c5b>
2013-04-04 23:35:40 +0000

    Skeleton for scheduler api

---
+++ lib/MetaScheduler/Torque.pm
@@ -0,0 +1,27 @@
+=head1 NAME
+
+    MetaScheduler::Torque
+
+=head1 DESCRIPTION
+
+    Interface to Torque/PBS
+
+    Not invoked on it's own, an internal object of Pipeline
+
+    Maybe should be subclassed from something more general
+    to create a standardized API to different schedulers? (bugaboo?)
+
+=head1 AUTHOR
+
+    Matthew Laird
+    Brinkman Laboratory
+    Simon Fraser University
+    Email: lairdm@sfu.ca
+
+=head1 LAST MAINTAINED
+
+    April 4, 2013
+
+=cut
+
+1;
