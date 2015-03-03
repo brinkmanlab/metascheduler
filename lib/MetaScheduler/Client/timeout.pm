0530f2f77a54da91a40e81620cf818be9cb99f19
lairdm <lairdm@sfu.ca>
2015-03-03 10:15:43 -0800

    New timeout commands

---
+++ lib/MetaScheduler/Client/timeout.pm
@@ -0,0 +1,131 @@
+=head1 NAME
+
+    MetaScheduler::Client::timeout
+
+=head1 DESCRIPTION
+
+    Object for handling changing the cache timeout to the scheduler
+    Should be called from the client script and gets it's
+    configuration from the @ARGV parameters
+
+=head1 SYNOPSIS
+
+    use MetaScheduler::Client::timeout;
+    MetaScheduler::Client::timeout->initialize($host, $port, @ARGV);
+
+=head1 AUTHOR
+
+    Matthew Laird
+    Brinkman Laboratory
+    Simon Fraser University
+    Email: lairdm@sfu.ca
+
+=head1 LAST MAINTAINED
+
+    March 3, 2015
+
+=cut
+
+package MetaScheduler::Client::timeout;
+
+use strict;
+use warnings;
+use JSON;
+use MetaScheduler::Client;
+
+use Getopt::Long;
+
+my $protocol_version = '1.0';
+
+sub initialize {
+    my ($self, $host, $port, @ARGV) = @_;
+
+    my $user; my $passwd;
+    my $help; my $raw;
+
+    my $timeout = shift @ARGV;
+
+    my $res = GetOptions ("raw"      => \$raw,
+			  "user=s"   => \$user,
+			  "passwd=s" => \$passwd,
+			  "help"  => \$help,
+	)
+	or die "Error in command line arguments";
+
+    if($help) {
+	usage();
+    }
+
+    die "Error, no user or password"
+	unless($user && $passwd);
+
+    usage()
+	unless($timeout =~ /\d+/);
+
+    my $client = MetaScheduler::Client->new();
+    $client->connect($host, $port);
+
+    my $str = buildReq($user, $passwd, $taskid);
+
+    print "Sending: $str\n";
+
+    my $ret = $client->send_req($str);
+
+    if($raw) {
+	print $ret;
+    } else {
+	printResults($ret);
+    }
+}
+
+sub usage {
+    print "Usage: $0 [timeout]\n";
+    exit; 
+}
+
+sub buildReq {
+    my $user = shift;
+    my $passwd = shift;
+    my $task_id = shift;
+
+    my $str = "{\n \"version\": \"$protocol_version\",\n";
+    $str .= " \"userid\": \"$user\",\n";
+    $str .= " \"password\": \"$passwd\",\n";
+    $str .= " \"action\": \"timeout\",\n";
+    $str .= " \"timeout\": \"$timeout\"\n";
+    $str .= "}\nEOF\n";
+
+    return $str;
+}
+
+# Interpret the results and make them pretty
+
+sub printResults {
+    my $json = shift;
+
+    my $res;
+    eval {
+	$res = decode_json($json);
+    };
+    if($@) {
+	die "Error, we can't interpret the results received from the server";
+    }
+
+    # If we received a 500 something went wrong with the request
+    if($res->{code} eq '500') {
+
+	print "Error, something went wrong with the request to the scheduler\n";
+	return;
+
+    # If we received a 404, that means there were no records found
+    } elsif($res->{code} eq '404') {
+	print "No jobs found\n";
+	return;
+    }
+
+    # Print the message
+    print "$res->{msg}\n";
+
+}
+
+1;
